b069135f5b57213f935e5ef110c03b4bd9e750a6
CardosoRaq <94082774+CardosoRaq@users.noreply.github.com>
2022-02-25 16:37:00 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,39 +2,54 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine, world, canvas;
-var ground;
-var caixa;
-var passaro;
-var tronco;
+var engine, world;
+var box1, pig1;
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    ground = Bodies.rectangle(600,390,1200,10,{isStatic: true});
-    World.add(world,ground);
+    
+    ground = new Ground(600,height,1200,20)
 
-    caixa = new Caixa (600,300,50,50);
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
 
-    passaro = new Passaro_maneiro(250,250);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
 
-    tronco = new Tronco(600,200,75, PI/2);
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
 
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
+    
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
 
-    caixa.display();
-    passaro.display();
-    tronco.display();
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
 
+    box5.display();
+    log4.display();
+    log5.display();
 
-    rectMode(CENTER);
-    fill("brown")
-    rect(ground.position.x,ground.position.y,1200,20);
-
+    bird.display();
 }
